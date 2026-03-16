5f126fea212298a0145e1a0790764291ab9ace2a
Greg Shenefelt <greg@shenefelt.net>
2026-03-16 11:56:55 -0700

    creating model and db for the cdr record

---
+++ database/migrations/2026_03_16_185139_create_cdr_record_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('CDRRecord', function (Blueprint $table) {
+            //
+            $table->id();
+            $table->date('call_date')->nullable();
+            $table->date('call_time')->nullable();
+            $table->string('caller')->nullable();
+            $table->string('answered_by')->nullable();
+            $table->softDeletes();
+            $table->nullableTimestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('CDRRecord', function (Blueprint $table) {
+            //
+        });
+    }
+};
